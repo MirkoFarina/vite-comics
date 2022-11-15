97ed465aea345436b00835e48abb89129ab8a5b2
MirkoFarina <MirkoFarina@outlook.com>
2022-11-15 17:44:21 +0100

    [add] stylezed dinamic stamp

--- src/components/StylizedNavBar.vue
+++ src/components/StylizedNavBar.vue
@@ -1,42 +1,61 @@
 <script>
 export default {
-    name: 'StylizedNavBar'
+    name: 'StylizedNavBar',
+    data (){
+        return {
+            contentNav: [
+                {
+                    imgUrl: 'buy-comics-digital-comics.png',
+                    text: 'digital comics',
+                    current: false
+                },
+                {
+                    imgUrl: 'buy-comics-merchandise.png',
+                    text: 'dc merchandise',
+                    current: false
+                },
+                {
+                    imgUrl: 'buy-comics-subscriptions.png',
+                    text: 'subscription',
+                    current: false
+                },
+                {
+                    imgUrl: 'buy-comics-shop-locator.png',
+                    text: 'comic shop locator',
+                    current: false
+                },
+                {
+                    imgUrl: 'buy-dc-power-visa.svg',
+                    text: 'dc power visa',
+                    current: false
+                }
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imageName){
+            return new URL(`../assets/img/${imageName}`, import.meta.url).href
+        }
+    }
 }
 </script>
 
 <template>
     <nav>
         <div class="container">
-            <div class="card">
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                <span>
-                    DIGITAL COMICS
-                </span>
-            </div>
-            <div class="card">
-                <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                <span>
-                    DC Merchandise
-                </span>
-            </div>
-            <div class="card">
-                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                <span>
-                   subscription
-                </span>
-            </div>
-            <div class="card">
-                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                <span>
-                    comic shop locator
-                </span>
-            </div>
-            <div class="card">
-                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                <span>
-                    dc power visa 
-                </span>
-            </div>
+            <ul>
+                <li
+                v-for="(link, index) in contentNav"
+                :key="index"
+                >
+                    <div class="img">
+                        <img :src="getImagePath(link.imgUrl)" :alt="link.text">
+                    </div>
+                    <a href="#">
+                        {{link.text}}
+                    </a>
+                </li>
+            </ul>
         </div>
     </nav>
 </template>
@@ -50,8 +69,25 @@ export default {
         background-color: $primary-color;
         padding: 20px 0;
         .container {
+           ul {
             @include centerFlex('horizontal');
             justify-content: space-around;
+            li {
+                @include centerFlex;
+                width: calc(100% / 5);
+                .img {
+                    width: 50%;
+                    img {
+                        transform: scale(.5);
+                    } 
+                }
+                a {
+                    font-size: .8rem;
+                    color: white;
+                    text-transform: uppercase;
+                }
+            }
+           }
             .card {
                 color: white;
                 
